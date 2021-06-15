2210c3661a830c806e0125ef17b6197557554efa
MistaMax <mmc_the_ace@yahoo.com>
2021-06-14 19:31:31 -0500

    added basic structure and functionality

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # BetBot
+# Setup
+Create your own config.json with the following variables:
+-token (the token for your bot)
+-prefix (the prefix you want for commands)
--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-hi
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
+const client = new Discord.Client();
+const config = require("./config.json");
+const llog = require("./datastruct/loadedlog.js");
+// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+client.config = config;
+client.fs = fs;
+client.llog = llog;
+client.currlog = llog.loadLog('./logs/default.log');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+//reads in the events
+fs.readdir("./evs/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const ev = require(`./evs/${file}`);
+    let evName = file.split(".")[0];
+    client.on(evName, ev.bind(null,client));
+  });
+});
+//reads in the commands
+client.commands = new Enmap();
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+//reads in the admin commands
+client.adminCommands = new Enmap();
+fs.readdir("./admin-commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./admin-commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+client.login(config.token);
