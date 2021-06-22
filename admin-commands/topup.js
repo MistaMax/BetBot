5feb0dfdbdb04ff06bc2457cee3062732849007b
MistaMax <mmc_the_ace@yahoo.com>
2021-06-21 19:33:23 -0500

    top up fix

--- admin-commands/topup.js
+++ admin-commands/topup.js
@@ -1,10 +1,13 @@
-const balance = require('../model/balance');
+const balance = require("../model/balance");
 
 exports.run = async (client, message, args) => {
-  if (message.author.id === client.config.ownerid || client.config.admins.includes(message.author.id)) {
-    await balance.addBalance(client, message.author.id, parseInt(args[0]));
-    message.reply(`Added $${args[0]} to your balance.`);
-  } else {
-    message.channel.send(`You do not have the necessary permissions ${message.author.tag}`);
-  }
-};
+    if(message.author.id===client.config.ownerid || client.config.admins.includes(message.author.id))
+    {
+        await balance.addBalance(client,message.author.id,parseInt(args[0]));
+        message.reply("Added $"+args[0]+" to your balance.");
+    }
+    else
+    {
+        message.channel.send("You do not have the necessary permissions " + message.author.tag);
+    }
+}
