422e042ec48ebcab739ceabcbb04b3989c8b0f8b
MistaMax <mmc_the_ace@yahoo.com>
2021-07-17 21:48:49 -0500

    The Intentional fail is stopping the pipeline as I hoped, commenting out

--- __test__/deploy.test.js
+++ __test__/deploy.test.js
@@ -1,7 +1,9 @@
-
-//set timeouts
 /*jest.setTimeout(2);
 it("Intentional Fail"){
     const bic = "hell"
     expect(bic).toEqual(null);
 }*/
+it("Test addition"){//just a simple test
+    const a = 1+1;
+    expect(a).toEqual(2);
+}
